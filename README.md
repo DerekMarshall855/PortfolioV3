7f30bcb9ad517a809d440f43855db1ba2b8d8108
DerekMarshall855 <derekmarshall855@gmail.com>
2021-12-02 19:19:39 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Personal Portfolio
+
+My personal Portfolio containing multiple projects, a description of me, and a basic overview of my skills.
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+
+import { SocialIcons } from '../Header/HeaderStyles';
+import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:519-369-7459">519-369-7459</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:derekmarshall855@gmail.com">derekmarshall855@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>
+            Improving my skills one project at a time.
+          </Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/derekmarshall855">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://linkedin.com/in/derekmarshall855">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
