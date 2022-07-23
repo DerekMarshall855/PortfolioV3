3da215de61259842f03579dfb1d41b4faeb86a1c
derekmarshall855 <derekmarshall855@gmail.com>
2022-07-22 20:40:36 -0400

    More hero edits

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,10 +14,10 @@ const Hero = () => (
       <SectionText>
         My name is Derek Marshall, I am a detail-oriented and self-motivated graduate with experience in
         software development. I graduated from Wilfrid Laurier University's Computer Science and Psychology double degree program in April 2021, with a cumulative GPA of 3.7.
-        I've worked in industry as a full stack developer for over a year, and am currently doing the same at the Government of Canada - IRCC.
-        Currently my goal is to keep improving in a wide variety of langauges and technologies so I can keep pursuing my passion for code.
-        My best languages right now are Typescript and Go, but I have experience in many others, and learn new ones quickly.
-        I am always open to ideas or offers of employment, and I encourage anyone to reach out to me on linkedin!
+        I've worked in industry as a full stack developer for over a year, and am currently employed by the Government of Canada - IRCC.
+        Presently, my goal is to continue improving in a wide variety of langauges and technologies so I can keep pursuing my passion for code.
+        My best languages are Typescript and Go, but I have experience in many others, and learn new ones quickly.
+        I am always open to new and interesting ideas and conversation, so I encourage anyone to reach out to me on linkedin!
       </SectionText>
       <Button onClick={() => window.location = 'https://github.com/derekmarshall855'}>Learn More</Button>
       <SectionDivider />
